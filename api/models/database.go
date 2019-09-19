deae42826091eace202495c352920586fc8e778a
James Woo <jamesjaywoo@gmail.com>
2019-09-19 17:41:38 -0400

    Not on vacation anymore

--- api/models/database.go
+++ api/models/database.go
@@ -45,28 +45,28 @@ func init() {
 	db.AutoMigrate(&Task{})
 
 	// Seed database with task
-	// var task Task
-	// db.Debug().FirstOrCreate(&task, Task{
-	// 	Schedule: "30 10 ? * MON,TUE,WED,THU,FRI",
-	// 	Type:     "fade",
-	// 	Data:     `{"start_color":{"r":0,"g":-50,"b":-120},"end_color":{"r":255,"g":130,"b":40},"start_intensity":0,"end_intensity":1,"duration":1800000}`,
-	// })
-	// var task2 Task
-	// db.Debug().FirstOrCreate(&task2, Task{
-	// 	Schedule: "0 11 ? * MON,TUE,WED,THU,FRI",
-	// 	Type:     "clear",
-	// })
-	// var task3 Task
-	// db.Debug().FirstOrCreate(&task3, Task{
-	// 	Schedule: "30 11 ? * SAT,SUN",
-	// 	Type:     "fade",
-	// 	Data:     `{"start_color":{"r":0,"g":-50,"b":-120},"end_color":{"r":255,"g":130,"b":40},"start_intensity":0,"end_intensity":1,"duration":1800000}`,
-	// })
-	// var task4 Task
-	// db.Debug().FirstOrCreate(&task4, Task{
-	// 	Schedule: "0 13 ? * SAT,SUN",
-	// 	Type:     "clear",
-	// })
+	var task Task
+	db.Debug().FirstOrCreate(&task, Task{
+		Schedule: "30 10 ? * MON,TUE,WED,THU,FRI",
+		Type:     "fade",
+		Data:     `{"start_color":{"r":0,"g":-50,"b":-120},"end_color":{"r":255,"g":130,"b":40},"start_intensity":0,"end_intensity":1,"duration":1800000}`,
+	})
+	var task2 Task
+	db.Debug().FirstOrCreate(&task2, Task{
+		Schedule: "0 11 ? * MON,TUE,WED,THU,FRI",
+		Type:     "clear",
+	})
+	var task3 Task
+	db.Debug().FirstOrCreate(&task3, Task{
+		Schedule: "30 11 ? * SAT,SUN",
+		Type:     "fade",
+		Data:     `{"start_color":{"r":0,"g":-50,"b":-120},"end_color":{"r":255,"g":130,"b":40},"start_intensity":0,"end_intensity":1,"duration":1800000}`,
+	})
+	var task4 Task
+	db.Debug().FirstOrCreate(&task4, Task{
+		Schedule: "0 13 ? * SAT,SUN",
+		Type:     "clear",
+	})
 
 	fmt.Printf("Initialized database %s\n", time.Now())
 }
